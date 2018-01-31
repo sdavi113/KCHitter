c29cbe6d6199225b9ec6951ad007781904083fd4
sdavi113 <sdavi113@stumail.jccc.edu>
2018-01-31 09:12:08 -0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KCHitterDADFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Graph()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us!";
+
+            return View();
+        }
+    }
+}
